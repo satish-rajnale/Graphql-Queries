f645cb4e76a6e2db9a7477d6b95a4f3e24018b34
satish-rajnale <satishrajnale98@gmail.com>
2021-09-06 20:09:01 +0530

    server up

--- SolidWithGraphql/server/index.ts
+++ SolidWithGraphql/server/index.ts
@@ -1,1 +1,34 @@
 import { GraphQLServer } from "graphql-yoga";
+
+let todos = [
+    { 
+        id : "1",
+        text: "Graphql + Solid Development",
+        done: false
+    }
+];
+
+const typeDefs = `
+    	type Todo {
+            id: ID!
+            done: Boolean!
+            text: String!
+        }
+        type Query {
+            getTodos : [Todo]!
+        }
+`;
+
+ const resolvers = {
+     Query : {
+        getTodos : () => {
+            return todos;
+        },
+     }
+ };
+
+ const server  = new GraphQLServer({
+     typeDefs, resolvers
+ });
+
+ server.start(() => console.log(`server running on: http://localhost:4000`))
