c0284dfa98a93d6bb2f85f6f0a916b22d7b306b8
satish-rajnale <satishrajnale98@gmail.com>
2021-09-06 21:05:33 +0530

    mutations

--- SolidWithGraphql/client/src/App.tsx
+++ SolidWithGraphql/client/src/App.tsx
@@ -1,12 +1,12 @@
 import type { Component } from "solid-js";
 import { createClient } from "@urql/core";
-import  {createResource} from "solid-js";
+import  {createResource, For, createSignal} from "solid-js";
 
 const client = createClient({
   url : "http://localhost:4000"
 });
 
-const [todos] = createResource(() => 
+const [todos, {refetch}] = createResource(() => 
   client.query(`
   query {
     getTodos{
@@ -16,13 +16,36 @@ const [todos] = createResource(() =>
     }
   }
   `).toPromise()
-    .then(({data}) => console.log(data))
+    .then(({data}) => data.getTodos)
   );
 
 const App: Component = () => {
+  const [text, setText] = createSignal("");
+
+  const onAdd = async () => {
+    await client.mutation(`
+      mutation($text: String!) {
+        addTodo: (text: $text){
+          id
+        }
+      }`,{ text: text() }
+      ).toPromise();
+      refetch();
+    setText("");
+  }
+
   return (
     <div >
-     Hello
+      <For each={todos()}>
+        {({id, done, text}) => <div>
+            <input type="checkbox" checked={done}/>
+            <span>{text}</span>
+          </div>}
+      </For>
+      <div>
+        <input type="text" value={text()} oninput={(e)=> setText(e.currentTarget.value)}/>
+        <button onclick={onAdd} value="Add"/>
+      </div>
     </div>
   );
 };
--- SolidWithGraphql/server/index.ts
+++ SolidWithGraphql/server/index.ts
@@ -17,6 +17,10 @@ const typeDefs = `
         type Query {
             getTodos : [Todo]!
         }
+        type Mutation {
+            addTodo(text: String!) : Todo
+            setDone(id:ID!, done: Boolean!): Todo
+        }
 `;
 
  const resolvers = {
@@ -24,6 +28,25 @@ const typeDefs = `
         getTodos : () => {
             return todos;
         },
+     },
+     Mutation: {
+        addTodo : (_:unknown, {text} :{text: string}) => {
+            const newTodo = {
+                id: String(todos.length +1),
+                text: text,
+                done: false,
+            };
+            todos.push(newTodo);
+            return newTodo
+        },
+        setDone: (_:unknown, {id, done}: {id:string, done:boolean}) => {
+            const todo = todos.find(todo => todo.id == id);
+            if(!todo){
+                throw new Error("Todo not found");
+            }
+            todo.done = done;
+            return todo;
+        }
      }
  };
 
