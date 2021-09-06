4a21f9367a292dd303b0bf94b0c35b3c66bacd06
satish-rajnale <satishrajnale98@gmail.com>
2021-09-06 20:20:11 +0530

    gettodos urql

--- SolidWithGraphql/client/src/App.tsx
+++ SolidWithGraphql/client/src/App.tsx
@@ -1,25 +1,28 @@
 import type { Component } from "solid-js";
+import { createClient } from "@urql/core";
+import  {createResource} from "solid-js";
 
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+const client = createClient({
+  url : "http://localhost:4000"
+});
+
+const [todos] = createResource(() => 
+  client.query(`
+  query {
+    getTodos{
+      id
+      done
+      text
+    }
+  }
+  `).toPromise()
+    .then(({data}) => console.log(data))
+  );
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div >
+     Hello
     </div>
   );
 };
